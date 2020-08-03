68b993e539e89fe4a0c219abbbb3c6d0202ca78b
Tyler Rhodes <tylerlrhodes@gmail.com>
2020-08-03 06:55:02 -0400

    updates

--- deploy.ps1
+++ deploy.ps1
@@ -9,7 +9,12 @@ hugo
 
 set-location "public\"
 
-Write-S3Object -BucketName tylerrhodes.net -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
+#Write-S3Object -BucketName tylerrhodes.net -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
+
+Write-S3Object -BucketName bagombo.org -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
+
+Write-S3Object -BucketName www.bagombo.org -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
+
 
 #https://blogs.technet.microsoft.com/heyscriptingguy/2015/11/05/generate-random-letters-with-powershell/
 $callerReference = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
