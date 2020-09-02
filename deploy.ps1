67028b50b82656b46d1aca0f2670a02d90246c06
Tyler Rhodes <tylerlrhodes@gmail.com>
2020-09-02 17:55:26 -0400

    updates to deploy

--- deploy.ps1
+++ deploy.ps1
@@ -5,20 +5,34 @@ Import-Module AWSPowerShell.NetCore
         
 Remove-Item "public" -Recurse -Force
 
+git checkout cloudfront
+
 hugo 
 
 set-location "public\"
 
 Write-S3Object -BucketName tylerrhodes.net -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
 
+#https://blogs.technet.microsoft.com/heyscriptingguy/2015/11/05/generate-random-letters-with-powershell/
+$callerReference = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
+New-CFInvalidation -DistributionId E2S8RAXJEUMBUD -InvalidationBatch_CallerReference $callerReference -Paths_Item "/*" -Paths_Quantity 1 -region "us-east-1" -ProfileName HugoProfile
+
+
+
+set-location ".."
+
+remove-item "public" -Recurse -Force
+
+git checkout cloudfare
+
+hugo
+
+set-location "public\"
+
 Write-S3Object -BucketName bagombo.org -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
 
 Write-S3Object -BucketName www.bagombo.org -Folder . -Recurse -KeyPrefix \ -CannedACLName public-read -ProfileName HugoProfile
 
-#https://blogs.technet.microsoft.com/heyscriptingguy/2015/11/05/generate-random-letters-with-powershell/
-$callerReference = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
-
-New-CFInvalidation -DistributionId E2S8RAXJEUMBUD -InvalidationBatch_CallerReference $callerReference -Paths_Item "/*" -Paths_Quantity 1 -region "us-east-1" -ProfileName HugoProfile
 
 Pop-Location
 
